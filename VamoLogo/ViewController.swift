db31a61932ce378dd63045430499adbbb3dbece4
Leonardo Mello <leonardomello2100@gmail.com>
2020-08-06 11:33:24 -0300

    adding trained data

--- VamoLogo/ViewController.swift
+++ VamoLogo/ViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func takephoto(){
+        let imagePickerActionSheet =
+          UIAlertController(title: "Snap/Upload Image",
+                            message: nil,
+                            preferredStyle: .actionSheet)
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+          let cameraButton = UIAlertAction(
+            title: "Take Photo",
+            style: .default) { (alert) -> Void in
+              let imagePicker = UIImagePickerController()
+              imagePicker.delegate = self
+              imagePicker.sourceType = .camera
+              imagePicker.mediaTypes = [kUTTypeImage as String]
+              self.present(imagePicker, animated: true, completion: {
+              })
+          }
+          imagePickerActionSheet.addAction(cameraButton)
+        }
+
+        // 3
+        let libraryButton = UIAlertAction(
+          title: "Choose Existing",
+          style: .default) { (alert) -> Void in
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+        }
+        imagePickerActionSheet.addAction(libraryButton)
+
+        // 4
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel)
+        imagePickerActionSheet.addAction(cancelButton)
+
+        // 5
+        present(imagePickerActionSheet, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 1
+        guard let selectedPhoto =
+          info[.originalImage] as? UIImage else {
+            dismiss(animated: true)
+            return
+        }
+        // 3
+        dismiss(animated: true) {
+           // self.performImageRecognition(selectedPhoto)
+        }
+
+
     }
 
-
 }
-
