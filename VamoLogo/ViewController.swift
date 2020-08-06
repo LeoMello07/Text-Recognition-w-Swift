1e1ac7816c47eb00d9aeb37ded901d0ebecc45dc
Leonardo Mello <leonardomello2100@gmail.com>
2020-08-06 10:55:59 -0300

    Initial Commit

---
+++ VamoLogo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VamoLogo
+//
+//  Created by Leonardo Mello on 06/08/20.
+//  Copyright © 2020 Leonardo Mello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
